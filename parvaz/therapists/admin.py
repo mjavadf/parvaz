00af48daf52e5226219aa4832ce294782426d5e5
mjavadf <javad.me1376@gmail.com>
2025-03-25 18:19:53 +0100

    Add Therapist and Speciality models with admin registration

--- parvaz/therapists/admin.py
+++ parvaz/therapists/admin.py
@@ -1,1 +1,14 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Speciality
+from .models import Therapist
+
+
+@admin.register(Therapist)
+class TherapistAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Speciality)
+class SpecialityAdmin(admin.ModelAdmin):
+    pass
--- parvaz/therapists/apps.py
+++ parvaz/therapists/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TherapistsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "therapists"
+    name = "parvaz.therapists"
